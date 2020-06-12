df52b0c7c6a1992be9f7aa54a47c987dd382acc5
Bruno Wu <wubruno@outlook.com>
2020-06-12 16:01:35 +0000

    Sign in complete

---
+++ README.md
@@ -0,0 +1,3 @@
+# Create secret
+
+`kubectl create secret generic jwt-secret --from-literal=JWT_KEY=asdf`
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -11,7 +12,14 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true); // trust nginx where calls could come from
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // no need to encrypt
+    secure: true, // must be in https connection
+  }),
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -25,6 +33,9 @@ app.all('*', () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
