47b889352b3552f8fbe51e070eb8848493ba0c3d
Bruno Wu <wubruno@outlook.com>
2020-06-14 12:36:40 +0000

    Client side complete

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,21 @@
-export default () => {
-  return <h1>Landing Page</h1>;
+import axios from 'axios';
+import buildClient from '../api/build-client';
+
+// Cannot make any requests or anything in here
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are NOT signed in</h1>
+  );
 };
+
+// Server side rendering of components that are required
+// req same as the request from the browser
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
+};
+
+export default LandingPage;
