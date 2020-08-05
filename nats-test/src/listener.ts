1d78db633ec319277a116ba2427979a1974f2797
Bruno Wu <wubruno@outlook.com>
2020-08-05 19:53:06 +0000

    Added listeners

---
+++ nats-test/src/listener.ts
@@ -0,0 +1,26 @@
+import nats, { Message } from 'node-nats-streaming';
+import { type } from 'os';
+import { randomBytes } from 'crypto';
+
+console.clear();
+
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Listener connected to NATS');
+
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'orders-service-queue-group',
+  );
+
+  subscription.on('message', (msg: Message) => {
+    const data = msg.getData();
+
+    if (typeof data === 'string') {
+      console.log(`Recevied event #${msg.getSequence()}, with data: ${data}`);
+    }
+  });
+});
---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,23 @@
+import nats from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+console.clear();
+
+// The client
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Publisher connected to NATS');
+
+  const data = JSON.stringify({
+    id: '123',
+    title: 'concert',
+    price: 20,
+  });
+
+  stan.publish('ticket:created', data, () => {
+    console.log('Event published');
+  });
+});
