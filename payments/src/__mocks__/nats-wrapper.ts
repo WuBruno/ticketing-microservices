b21c616bfc3fce6c09f839472020ad5dfc9e0a5e
Bruno Wu <wubruno@outlook.com>
2020-08-29 22:01:57 +0000

    Basics done

---
+++ payments/src/__mocks__/nats-wrapper.ts
@@ -0,0 +1,13 @@
+// Allows to write some fake implementation of something in the directory
+
+export const natsWrapper = {
+  client: {
+    publish: jest
+      .fn()
+      .mockImplementation(
+        (subject: string, data: string, callback: () => void) => {
+          callback();
+        },
+      ),
+  },
+};
---
+++ payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,22 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@bwtickets/common';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../models/order';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      userId: data.userId,
+      price: data.ticket.price,
+      version: data.version,
+      status: data.status,
+    });
+    await order.save();
+
+    msg.ack();
+  }
+}
