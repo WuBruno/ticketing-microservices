87f6c0540808063a7ae06b4eb7b6a9baff8ad8fe
Bruno Wu <wubruno@outlook.com>
2020-08-31 23:00:56 +0000

    small fixes

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,8 +4,7 @@ export default ({ req }) => {
   if (typeof window === 'undefined') {
     // In the server
     return axios.create({
-      baseURL:
-        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://www.brunowu.me',
       headers: req.headers,
     });
   } else {
